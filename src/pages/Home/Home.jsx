c8c9781b7bc31955686c9b132d68445b0de13218
Amit <64500653+Amit1Paz@users.noreply.github.com>
2022-09-07 00:58:21 +0300

    Feat(Home) tabs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,36 @@
+import { Box, Tab, Tabs, Typography } from '@mui/material';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import Header from '../../components/Header/Header';
 import { fetchAllCharacters } from '../../util/services/RickAndMorty.service';
 import CharacterTable from './CharacterTable';
 
 function Home() {
+  const [tabValue, setTabValue] = useState(0);
+
   const { data, status } = useQuery('characters', () => fetchAllCharacters());
+
+  const handleChangeTab = (e, newValue) => setTabValue(newValue);
+
+  if (status === 'error') return <Typography color='error'>Error</Typography>;
 
   return (
     <>
-      <Header />;
-      <CharacterTable data={data} status={status} />
+      <Header />
+      <Box as='main'>
+        <Tabs
+          value={tabValue}
+          onChange={handleChangeTab}
+          variant='fullWidth'
+          sx={{ marginBottom: '3rem' }}
+        >
+          <Tab label='Part One' sx={{ color: 'black' }} />
+          <Tab label='Part Two' sx={{ color: 'black' }} />
+        </Tabs>
+        <Box px='20vw'>
+          {tabValue === 0 && <CharacterTable data={data} status={status} />}
+        </Box>
+      </Box>
     </>
   );
 }
